79c4ebcd3977c4e943cc33c8908a087525c691eb
DaniVSainz <danivsainz@gmail.com>
2017-03-30 01:06:49 -0400

    Change appointment_form + appointments to allow submitting

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -8,11 +8,17 @@ var AppointmentForm = React.createClass({
     this.props.onUserInput(obj);
   },
 
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.onFormSubmit();
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make a new appointment</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input name='title' placeholder='Appointment Title'
             value={this.props.input_title}
             onChange={this.handleChange} />
