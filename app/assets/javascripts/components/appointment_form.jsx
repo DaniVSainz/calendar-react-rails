6782b7e1e9293bc9f854cb43866f0b5a39d9f649
DaniVSainz <danivsainz@gmail.com>
2017-03-30 00:51:21 -0400

    Add getInitialState add set state , change forms and components

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,14 +1,27 @@
 var AppointmentForm = React.createClass({
-  render: function () {
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    console.log(obj);
+    console.log(this.props.onUserInput);
+    this.props.onUserInput(obj);
+  },
+
+  render: function() {
     return (
       <div>
-        <h2>Make a new Appointment </h2>
+        <h2>Make a new appointment</h2>
         <form>
-          <input name='title' placeholder='Appointment Title' />
-          <input name='appt_time' placeholder='Date & Time' />
+          <input name='title' placeholder='Appointment Title'
+            value={this.props.input_title}
+            onChange={this.handleChange} />
+          <input name='appt_time' placeholder='Date and Time'
+            value={this.props.input_appt_time}
+            onChange={this.handleChange}  />
           <input type='submit' value='Make Appointment' />
         </form>
       </div>
     )
   }
-})
+});
