8a977f7745a64a3b1122c509e625b17e7aa89354
DaniVSainz <danivsainz@gmail.com>
2017-03-31 01:12:30 -0400

    Add arrow functions and remove .bind(this)

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -13,16 +13,16 @@ class Appointments extends React.Component {
   }
 
   handleFormSubmit() {
-    var appointment = {title: this.state.title, appt_time: this.state.appt_time};
+    const appointment = {title: this.state.title, appt_time: this.state.appt_time};
     $.post('/appointments',
             {appointment: appointment})
-          .done(function(data) {
+          .done((data) => {
             this.addNewAppointment(data);
-          }.bind(this));
+          });
   }
 
   addNewAppointment(appointment) {
-    var appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
+    const appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
     this.setState({
       appointments: appointments.sort(function(a,b){
         return new Date(a.appt_time) - new Date(b.appt_time);
@@ -35,8 +35,8 @@ class Appointments extends React.Component {
       <div>
         <AppointmentForm input_title={this.state.title}
           input_appt_time={this.state.appt_time}
-          onUserInput={this.handleUserInput.bind(this)}
-          onFormSubmit={this.handleFormSubmit.bind(this)} />
+          onUserInput={(obj) =>this.handleUserInput(obj)}
+          onFormSubmit={() => this.handleFormSubmit()} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
