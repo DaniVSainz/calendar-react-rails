27a429bdd485be1bf693c5282b84ae416baf8757
DaniVSainz <danivsainz@gmail.com>
2017-03-31 08:00:23 -0400

    Rename Client/Hellowworld to Appointments, change webpack.config.js config to include appointments registration, move components into client folder from app assets javascripts

---
+++ client/app/bundles/Appointments/startup/registration.jsx
@@ -0,0 +1,7 @@
+import ReactOnRails from "react-on-rails";
+import Appointments from "../components/appointments";
+
+// This is how react_on_rails can see the Appointments in the browser.
+ReactOnRails.register({
+  Appointments
+});
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :appointments
   resources :navigator
   get 'appointments/index'
-  root 'navigator#index'
+  root 'appointments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
