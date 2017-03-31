b33ecee3fc146109c2c7f2f41850269e91376c25
DaniVSainz <danivsainz@gmail.com>
2017-03-31 06:13:29 -0400

    Changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :appointments
+  resources :navigator
   get 'appointments/index'
-  root 'appointments#index'
+  root 'navigator#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
