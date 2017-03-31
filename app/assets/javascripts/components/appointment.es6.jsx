64cd57413f614f5b5e561befd157b5d85f2b122a
DaniVSainz <danivsainz@gmail.com>
2017-03-31 01:24:19 -0400

    Create stateless functional components

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,8 +1,5 @@
-const Appointment = ({appointment}) => {
-    return(
-      <div>
-        <h3>{appointment.title}</h3>
-        <p>{moment(appointment.appt_time).format('MMMM Do YYYY, h:mm:ss a')}</p>
-      </div>
-    )
-}
+const Appointment = ({appointment}) =>
+  <div>
+    <h3>{appointment.title}</h3>
+    <p>{moment(appointment.appt_time).format('MMMM Do YYYY, h:mm:ss a')}</p>
+  </div>
