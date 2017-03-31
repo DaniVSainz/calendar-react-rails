71bd74969a065a64b4b02332e75a6ae4865ba9e2
DaniVSainz <danivsainz@gmail.com>
2017-03-31 08:15:21 -0400

    Transition 90% complete, works online @localhost:3000

--- client/app/bundles/Appointments/components/appointment_form.jsx
+++ client/app/bundles/Appointments/components/appointment_form.jsx
@@ -1,7 +1,11 @@
-class AppointmentForm extends React.Component{
-  handleChange (e) {
+import React from "react";
+import Datetime from "react-datetime";
+import Label from "./label"
+
+export default class AppointmentForm extends React.Component {
+  handleChange(e) {
     const name = e.target.name;
-    obj = {};
+    const obj = {};
     obj[name] = e.target.value;
     this.props.onUserInput(obj);
   }
@@ -12,32 +16,27 @@ class AppointmentForm extends React.Component{
   }
 
   setApptTime(e) {
-    const name = 'appt_time';
+    const name = "appt_time";
     const obj = {};
-    if(obj[name] = e.toDate()) {
+    if(obj[name] = e.toDate()){
       this.props.onUserInput(obj);
     }
   }
 
   render() {
     const inputProps = {
-      name: 'appt_time'
+      name: "appt_time"
     };
-
     return (
-      <div>
-        <h2>Make a new appointment</h2>
-        <br/>
-        <form onSubmit={(event) => this.handleSubmit(event)}>
-          <input name='title' placeholder='Appointment Title'
-            value={this.props.input_title}
-            onChange={(event) => this.handleChange(event)} />
-
-          <Datetime input={false} open={true} inputProps={inputProps}
-            value={this.props.appt_time}
-            onChange={(event) => this.setApptTime(event)} />
-
-          <input type='submit' value='Make Appointment' className='submit-button' />
+      <div id="new-appt">
+        <h5>Make a new appointment</h5>
+        <form className="form" onSubmit = { (e) => this.handleSubmit(e) }>
+          <input name="title" className="form-control"
+                 value={this.props.input_title} onChange={ (e) => this.handleChange(e) } />
+          <Label label="Choose date and time" />
+          <Datetime input={false} open={true} inputProps={inputProps} value={this.props.appt_time}
+                    onChange={ (e) => this.setApptTime(e) } />
+          <input type="submit" value="Make Appointment" className="btn btn-primary" />
         </form>
       </div>
     )
--- client/app/bundles/Appointments/components/label.jsx
+++ client/app/bundles/Appointments/components/label.jsx
@@ -1,7 +1,9 @@
-class Label extends React.Component {
+import React from "react"
+
+export default class Label extends React.Component {
   render () {
     return (
-      <div>
+      <div className="vspace-10">
         <div>{this.props.label}</div>
       </div>
     );
