19aa7179e5e63a694e6f21c97eee871de964e35c
DaniVSainz <danivsainz@gmail.com>
2017-03-31 01:29:16 -0400

    add class appointment in appointment.jsx and create .appointment css

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,5 +1,5 @@
 const Appointment = ({appointment}) =>
-  <div>
+  <div className='appointment'>
     <h3>{appointment.title}</h3>
     <p>{moment(appointment.appt_time).format('MMMM Do YYYY, h:mm:ss a')}</p>
   </div>
