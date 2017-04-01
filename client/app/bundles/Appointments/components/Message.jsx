9b3316d512ff6d749959bf305f6aca5d061c7b12
DaniVSainz <danivsainz@gmail.com>
2017-04-01 00:53:58 -0400

    Add message component, registered it added it to index hello world

---
+++ client/app/bundles/Appointments/components/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Message = (props,_railsContext) => (
+  <div>
+    Text: Blah Blah
+  </div>
+);
+
+export default Message;
--- client/app/bundles/Appointments/startup/registration.jsx
+++ client/app/bundles/Appointments/startup/registration.jsx
@@ -1,7 +1,11 @@
 import ReactOnRails from "react-on-rails";
 import Appointments from "../components/appointments";
+import HelloWorld from "../components/HelloWorld";
+import Message from "../components/Message"
 
 // This is how react_on_rails can see the Appointments in the browser.
 ReactOnRails.register({
-  Appointments
+  Appointments,
+  HelloWorld,
+  Message
 });
